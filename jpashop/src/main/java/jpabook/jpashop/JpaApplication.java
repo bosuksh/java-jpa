f05f6847dcc163b5a820103f4db3c5202d587fcf
Lee SangHyup <bosuksh@gmail.com>
2021-02-10 22:52:42 +0900

    feat: Entity Model 생성

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaApplication.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaApplication {
+
+  public static void main(String[] args) {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpashop");
+
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    EntityTransaction transaction = entityManager.getTransaction();
+    transaction.begin();
+    try{
+      // 비영속 상태
+
+      transaction.commit();
+    }catch (Exception e) {
+      transaction.rollback();
+    }finally {
+      entityManager.close();
+      entityManagerFactory.close();
+    }
+
+  }
+}
+
