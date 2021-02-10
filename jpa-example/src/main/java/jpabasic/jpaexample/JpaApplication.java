056cca95aaf616023d568a3ae32d47898c0e1331
Lee SangHyup <bosuksh@gmail.com>
2021-02-10 21:54:13 +0900

    chore: 이전 자바 코드 위치 변경

---
+++ jpa-example/src/main/java/jpabasic/jpaexample/JpaApplication.java
@@ -0,0 +1,33 @@
+package jpabasic.jpaexample;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaApplication {
+
+  public static void main(String[] args) {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    EntityTransaction transaction = entityManager.getTransaction();
+    transaction.begin();
+    try{
+      // 비영속 상태
+      Member member = new Member();
+      member.setId(1L);
+      member.setRoleType(RoleType.ADMIN);
+      entityManager.persist(member);
+      transaction.commit();
+    }catch (Exception e) {
+      transaction.rollback();
+    }finally {
+      entityManager.close();
+      entityManagerFactory.close();
+    }
+
+  }
+}
+
