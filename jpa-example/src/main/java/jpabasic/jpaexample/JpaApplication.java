5a25712e64ecd1cbfe442f3c3948a9e48662a3c8
Lee SangHyup <bosuksh@gmail.com>
2021-02-14 02:16:17 +0900

    feat: JPA Proxy 사용 (getReference())

--- jpa-example/src/main/java/jpabasic/jpaexample/JpaApplication.java
+++ jpa-example/src/main/java/jpabasic/jpaexample/JpaApplication.java
@@ -1,5 +1,7 @@
 package jpabasic.jpaexample;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -19,12 +21,25 @@ public class JpaApplication {
       Member member = new Member();
 
       member.setUsername("doflamingo");
-      member.setCreatedBy("doflamingo");
-      member.setCreatedDateTime(LocalDateTime.now());
       entityManager.persist(member);
+      entityManager.flush();
+      entityManager.clear();
+
+      Member referenceMember = entityManager.getReference(Member.class, member.getId());
+      System.out.println("member = " + referenceMember.getClass());
+      Member findMember = entityManager.find(Member.class, member.getId());
+      System.out.println("isLoaded = " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(Member.class));
+      System.out.println("findMember.getUsername() = " + findMember.getUsername());
+      System.out.println("referenceMember.getUsername() = " + referenceMember.getUsername());
+
+      // instance of
+      System.out.println("findMember == referenceMember = " + (referenceMember.getClass() == Member.class));
+
+
       transaction.commit();
     }catch (Exception e) {
       transaction.rollback();
+      System.out.println(e.getMessage());
     }finally {
       entityManager.close();
       entityManagerFactory.close();
